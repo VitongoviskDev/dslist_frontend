1158e6c445890c4230bc938bd3b1a89446905a51
VitorCamposDev <v_campos_dev@outlook.com>
2025-04-06 19:44:56 -0300

    building

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 import GamesList from './Components/GameList/Index';
 import GameSection from './Components/GameSection';
 
--- src/Components/GameSection/index.tsx
+++ src/Components/GameSection/index.tsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import GameDTO from '../../types/GameDTO';
-import axios from 'axios';
 
 import './GameSection.css'
 import api from '../../axiosConfig';
 
-interface GameSectionProps{
+interface GameSectionProps {
   gameId: number;
 }
 
 const GameSection = (props: GameSectionProps) => {
-  const [game, setGame] = useState<GameDTO|null>(null);
+  const [game, setGame] = useState<GameDTO | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const fecthGame = async () => {
       try {
         const response = await api.get<GameDTO>(`/games/${props.gameId}`);
@@ -31,21 +30,23 @@ const GameSection = (props: GameSectionProps) => {
 
   return (
     <section className="game_section">
-        <div className="container">
-          {
-            game && 
-            <>
-              <div className="image_container">
-                <img src={game.imgUrl} alt={`${game.title } thumbnail`} />
-              </div>
-              <div className="text_container">
-                  <h1 className="game_title">{game.title}</h1>
-                  <p className="game_year">{game.year}</p>
-                  <p className="full_description">{game.longDescription}</p>
-              </div>
-            </>
-          }
-        </div>
+      <div className="container">
+        {
+          loading ? <p>Carregando...</p> :
+          error ? <p>ERRO: {error}</p> :
+          game &&
+          <>
+            <div className="image_container">
+              <img src={game.imgUrl} alt={`${game.title} thumbnail`} />
+            </div>
+            <div className="text_container">
+              <h1 className="game_title">{game.title}</h1>
+              <p className="game_year">{game.year}</p>
+              <p className="full_description">{game.longDescription}</p>
+            </div>
+          </>
+        }
+      </div>
     </section>
   )
 }
